961567885b3398deaa89d778c52e5177a57de036
Michael <michaelpaul3154@gmail.com>
2026-01-28 18:29:20 +0100

    Update

--- portfoilo/urls.py
+++ portfoilo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('project_app/', views.all_projects, name='all_projects'),
 ]
--- portfoilo/views.py
+++ portfoilo/views.py
@@ -2,20 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import MyProject, ContactMessage
 
-
-
+# 1. HOMEPAGE VIEW (Only shows first 3 projects)
 def index(request):
-  
-    projects = MyProject.objects.all()
-    
+    # Slice [:3] to get only the 3 most recent projects
+    projects = MyProject.objects.all().order_by('-date_added')[:3] 
     
     if request.method == 'POST':
-        
         name = request.POST.get('name')
         email = request.POST.get('email')
         subject = request.POST.get('subject')
         message = request.POST.get('message')
-        
         
         ContactMessage.objects.create(
             name=name,
@@ -24,12 +20,16 @@ def index(request):
             message=message
         )
         
-      
         messages.success(request, 'Your message has been sent. Thank you!')
-        
-        
         return redirect('index')
 
-    
+    # Note: Using the path you provided in your snippet
     return render(request, 'project_app/index.html', {'projects': projects})
-# 
+
+# 2. NEW VIEW (Shows ALL projects)
+def all_projects(request):
+    # Fetch ALL projects here (no slicing)
+    projects = MyProject.objects.all().order_by('-date_added')
+    
+    # We will create this 'all_projects.html' file next
+    return render(request, 'project_app/all_projects.html', {'projects': projects})
